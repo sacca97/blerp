7b874274f5bae7c081824eb1e91b67edaf270966
sacca97 <tommaso.sacchetti@gmail.com>
2025-12-01 16:57:50 +0100

    minor fixes

--- apply_attacks_patch.sh
+++ apply_attacks_patch.sh
@@ -3,10 +3,12 @@
 CURRENT_DIR_COMMAND=$(pwd)
 
 cd repos/apache-mynewt-nimble
+git stash
 git checkout 675452b628
 
 git apply --reject --whitespace=fix "$CURRENT_DIR_COMMAND"/patches/blerp.patch
 
 cd "$CURRENT_DIR_COMMAND"
 cd repos/apache-mynewt-core
+git stash
 git apply --reject --whitespace=fix "$CURRENT_DIR_COMMAND"/patches/core.patch
